66696ce93f3fb78a8297fc58557aad8da970a472
Phu Paul <ptphu@monotos.biz>
2023-06-06 17:05:54 +0700

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# 3proxy-docker
+# Squid Docker
+
+## Add user
+
+  ```
+  docker exec 3proxy sh -c "/add3proxyuser.sh YOUR_USERNAEM YOUR_PASSWORD"
+  ```
+
+## Testing
+
+  ```
+  curl -v -x socks5://YOUR_USERNAEM:YOUR_PASSWORD@localhost:1081 http://www.google.com/
+  ```
+
+## Referent
+
+ - https://github.com/3proxy/3proxy
---
+++ volumes/3proxy/conf/add3proxyuser.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ $4 ]; then
+	echo bandlimin $4 $1 >> /etc/3proxy/conf/bandlimiters
+fi
+if [ $3 ]; then  
+	echo countin \"`wc -l /etc/3proxy/conf/counters|awk '{print $1}'`/$1\" D $3 $1 >> /etc/3proxy/conf/counters
+fi
+if [ $2 ]; then  
+	echo users \"$1:`/bin/mycrypt $$ $2`\" >> /etc/3proxy/conf/passwd
+else
+	echo usage: $0 username password [day_limit] [bandwidth]
+	echo "	"day_limit - traffic limit in MB per day
+	echo "	"bandwidth - bandwith in bits per second 1048576 = 1Mbps
+fi
+
